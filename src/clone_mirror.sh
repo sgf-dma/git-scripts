4ccdf7ef617143bcbcf6e7164794a7ea2f3746ee
sgf <sgf.dma@gmail.com>
2020-09-30 17:42:02 +0300

    new: clone_mirror. Add script for cloning 'mirror' repositories.
    If repository uses "sync remote" (for syncing everything) created with
    'git remote add --mirror=push ..', then during cloning some additional
    steps are necessary to make a proper clone.

---
+++ src/clone_mirror.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -euf
+
+repo_url="$1"
+sync_url="$2"
+repo="$(basename "$repo_url" .git)"
+tmp_repo="git_$repo"
+
+echo "Will use '$repo_url' as 'origin' remote."
+echo "Will use '$sync_url' as 'sync' remote (_mirror_ everything)."
+read -p "Continue? " x
+if [ "$x" != 'y' ]; then
+    echo "Exit."
+    exit 0
+fi
+
+if [ ! -d "$repo" ]; then
+    git clone --mirror "$sync_url" "$tmp_repo"
+    mkdir "$repo"
+    mv "$tmp_repo" "$repo/.git"
+    cd "$repo"
+    git config --bool core.bare false
+    git config --bool core.logallrefupdates true
+    git checkout master
+    git remote rename origin sync
+    git remote add origin "$repo_url"
+    git remote update origin
+    git push -u origin :
+else
+    echo "Repository directory '$repo' already exists."
+    exit 0
+fi
